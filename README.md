25171264612c972ebd54fd019372a4ef5572dad3
Ignacio Ito <ignaciotrabalho@gmail.com>
2018-09-11 00:17:28 -0400

    Test Fonction

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # IntroductionALaProgrammation
+
+C'est repo a été crée pour la discipline Introduction à la Programmation.
+
+Tous les exercises fait seront partagé là. 
---
+++ fonction2.c
@@ -0,0 +1,19 @@
+//
+//
+//
+// Autheur: Ignacio Ito
+//
+// Les Fonctions
+// 
+
+#include<stdio.h>
+#include<stdlib.h>
+
+void fonctionMienne(int i)
+{
+	i = 0;
+	while (i < 10){
+		printf("%d\n", i);
+		i++;
+	}
+}
