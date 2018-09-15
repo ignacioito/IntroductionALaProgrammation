f777ac5021ddeea9b4292a111d49416cc20c15e4
Ignacio Ito <ignaciotrabalho@gmail.com>
2018-09-15 19:27:05 -0400

    test commit
    test ssss

---
+++ testbool.c
@@ -0,0 +1,22 @@
+//
+
+//Teste booleano
+
+//
+
+#include<stdbool.h>
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+	bool a = true, b = false;
+
+	if (a){
+		printf("A variável a é verdadeira\n");
+	}	
+
+	if(!b) {
+		printf("A variável b é falsa\n");
+	}
+}
