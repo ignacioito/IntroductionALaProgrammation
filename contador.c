5e602eda7ab3815d73e588d2c73242ebb199e82f
Ignacio Ito <ignaciotrabalho@gmail.com>
2018-09-16 07:25:24 -0400

    contador

---
+++ contador.c
@@ -0,0 +1,30 @@
+//
+//
+//Author Ignacio Ito
+//Contador de 1 a 5
+//
+//
+#include<stdio.h>
+#include<stdlib.h>
+
+/*int main(int argc, char const *argv[])
+{
+	int a = 0;
+	while(a <= 5){
+		printf("%d\n", a);
+		a++;
+	}
+	return 0;
+}
+
+*/
+// omitindo o 0
+int main(int argc, char const *argv[])
+{
+	int a = 1;
+	while(a <= 5){
+		printf("%d\n", a);
+		a++;
+	}
+	return 0;
+}
