6f3531f61013fa21b39fba7f78dfd4676cd64796
ignacio <ignacioito@gmail.com>
2018-09-26 15:10:56 -0400

    new commit
    new commit avec des codes de l'ancien repo C_IntroProgrammation...

--- revision2.c
+++ revision2.c
@@ -6,6 +6,54 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+//Tabuada
+/*
+int main(int argc, char const *argv[])
+{
+	int n, i = 1, c;
+	printf("_______TabuadA_______\n");
+	printf("Disez le nombre: \n");
+	scanf(" %d", &n);
+	while(i <= 10){
+		int c = (n * i);
+		printf("%d * %d = %d\n", n, i, c);
+		i++;
+	}
+	return 0;
+}
+*/
+
+int main(int argc, char const *argv[])
+{
+	int n, i = 0, contNeg = 0;
+
+	printf("_______Compteur de Negatifs_______\n");
+
+	while(i <= 5){
+	printf("Tapez le numero: \n");
+	scanf("%d", &n);
+	if(n < 0){
+	contNeg = contNeg + 1;
+		}
+	i++;
+	}
+
+	printf("Le numero de negatifs c'est: %d\n", contNeg);
+	
+
+
+	
+}
+
+
+/*
+//Auteur Ignacio Ito
+//
+// Revision
+
+#include<stdio.h>
+#include<stdlib.h>
+
 
 /*
 int main(int argc, char const *argv[])
@@ -22,7 +70,7 @@ int main(int argc, char const *argv[])
 	return 0;
 }
 
-*/
+
 
 // Programm pour prendre 5 nombres et afficher le nombre de negatifs:
 
@@ -42,3 +90,5 @@ int main(int argc, char const *argv[])
 	printf("Le nombre de nombre negatifs c'est: %d\n", contNeg);
 	
 }
+
+*/
