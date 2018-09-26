5585bfb25c17c6caf44d6f9a0e11aea3aae2b461
Ignacio Ito <ignaciotrabalho@gmail.com>
2018-09-26 13:15:54 -0400

    factoriel version1

---
+++ factoriel.c
@@ -0,0 +1,16 @@
+//
+//
+
+//Auteur Ignacio Ito
+//Exercice Calcul de nombre factoriell
+
+//
+//
+
+#include<stdio.h>
+#include<stdlib.h>
+int main(int argc, char const *argv[])
+{
+	/* code */
+	return 0;
+}
