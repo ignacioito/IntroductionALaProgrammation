0fc4096cb2ac5920bec7504654d6f40fc77b7fc8
ignacio <ignacioito@gmail.com>
2018-09-26 15:39:26 -0400

    FACTORIEL
    New commit calcul de factoriel

---
+++ revision3.c
@@ -0,0 +1,34 @@
+//
+//
+//Auteur Ignacio Ito
+//Revision Repetition 3
+//
+// Calcule de numero factoriel
+// exemple 5:
+// 5 * 4 * 3 * 2 * 1 = 120
+
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(int argc, char const *argv[])
+{
+	int n, c = 1, i;
+	printf("-----Calcul de Numero Factoriel-----\n");
+	printf("Tapez le numero:\n");
+	scanf("%d", &n);
+	i = n;
+	while(i >= 1){
+		c = i * c;
+		i--;
+
+	}
+	printf("%d\n", c);
+/*
+	for(i = n; i >= 1; i--){
+		c = i * n;
+		printf("%d\n", c);
+	}
+*/
+		
+}
