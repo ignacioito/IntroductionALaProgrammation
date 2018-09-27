6038dfd470ab527de0cbb6c52a0d7ad6120c8c29
Ignacio Ito <ignaciotrabalho@gmail.com>
2018-09-27 00:14:30 -0400

    New Commit
    Test factoriel. Faites la correction du code

--- factoriel.c
+++ factoriel.c
@@ -11,6 +11,24 @@
 #include<stdlib.h>
 int main(int argc, char const *argv[])
 {
-	/* code */
-	return 0;
+	int n = 0, calcul, i;
+	char sn = 's';
+
+	printf("----Programme de Calcul de Numéro Factoriel----\n");
+	printf("Tapez un numéro pour savoir le factoriel\n");
+	scanf(" %d", &n);
+while(sn == 's'){
+	while(n >= 1){
+		printf("Tapez un numéro pour savoir le factoriel\n");
+		scanf(" %d", &n);
+		calcul = calcul * n;
+		n--;
+		printf("le factoriel de %d c'est %d.\n", n, calcul);
+	}
+	printf("Voulez vous continuer? (s/n)\n");
+	scanf(" %c", &sn);
 }
+	
+}
+
+
