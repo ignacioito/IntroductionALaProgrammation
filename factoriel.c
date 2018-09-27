52a8144cb407b2cca0073291990d9ac0bd4bfa2b
ignacio <ignacioito@gmail.com>
2018-09-27 15:01:41 -0400

    Salva código

--- factoriel.c
+++ factoriel.c
@@ -9,8 +9,25 @@
 
 #include<stdio.h>
 #include<stdlib.h>
+
 int main(int argc, char const *argv[])
 {
-	/* code */
-	return 0;
+	int n, c = 1, i;
+	printf("-----Calcul de Numero Factoriel-----\n");
+	printf("Tapez le numero:\n");
+	scanf("%d", &n);
+	i = n;
+	while(i >= 1){
+		c = i * c;
+		i--;
+
+	}
+	printf("%d\n", c);
+/*
+	for(i = n; i >= 1; i--){
+		c = i * n;
+		printf("%d\n", c);
+	}
+*/
+		
 }
