7785f0ffab330c0298f76f740f985f8aba230b64
Ignacio Ito <ignaciotrabalho@gmail.com>
2018-09-27 23:54:00 -0400

    atualização
    Código ainda com erro.
    
    Preciso corrigir o looping que permite continuar a calcular novo número fatorial ou encerrar o programa.

--- factoriel.c
+++ factoriel.c
@@ -11,24 +11,31 @@
 #include<stdlib.h>
 int main(int argc, char const *argv[])
 {
-	int n = 0, calcul, i;
+	int n = 0, calcul = 1, i;
 	char sn = 's';
 
 	printf("----Programme de Calcul de Numéro Factoriel----\n");
+
 	printf("Tapez un numéro pour savoir le factoriel\n");
 	scanf(" %d", &n);
+	
+	i = n;
 while(sn == 's'){
-	while(n >= 1){
+	printf("Voulez vous continuer? s/n\n");
+	scanf(" %c", &sn);
+		while(i>=1){
 		printf("Tapez un numéro pour savoir le factoriel\n");
 		scanf(" %d", &n);
-		calcul = calcul * n;
-		n--;
-		printf("le factoriel de %d c'est %d.\n", n, calcul);
-	}
-	printf("Voulez vous continuer? (s/n)\n");
-	scanf(" %c", &sn);
-}
+		
+		calcul = calcul * i;
 	
+	
+		i--;
+		
+		}
+}
+		printf("le factoriel de %d c'est %d.\n", n, calcul);
+
 }
 
 
