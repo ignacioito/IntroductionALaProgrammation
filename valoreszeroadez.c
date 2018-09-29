857d2a4d05fc0a49a749c599bdef5cd8b9cdbf95
Ignacio Ito <ignaciotrabalho@gmail.com>
2018-09-29 13:24:59 -0400

    Concluído
    =D

--- valoreszeroadez.c
+++ valoreszeroadez.c
@@ -10,3 +10,24 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+int main(int argc, char const *argv[])
+{
+	int n, sn = 0, cont = 0, imp = 0;
+
+	printf("- - - - Conteur des nombres entre 0 et 10 - - - -\n");
+
+	while(sn == 0){
+		printf("Tapez le nombre: \n");
+		scanf(" %d", &n);
+		if ((n >= 0) && (n <= 10)){	
+			if (n % 2 == 1){
+			imp++;
+		}
+			cont++;
+		}
+		printf("Voulez vous continuer? s: 0/n: 1\n");
+		scanf("%d", &sn);
+	}
+
+	printf("La quantité des nombres entre 0 et 10 sont: %d et les nombres impairs: %d\n", cont, imp);
+}
