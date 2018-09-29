fca1813a9eee3ae4c72e6f79a55964184426b2d2
Ignacio Ito <ignaciotrabalho@gmail.com>
2018-09-29 12:16:32 -0400

    valores de zero a dez
    Entrada de valores. O programa dirá quantos valores estão entre o intervalo de 0 a 10.
    Adicionalmente dirá quantos valores são negativos.

---
+++ valoreszeroadez.c
@@ -0,0 +1,12 @@
+//
+//
+// Auteur Ignacio Ito
+//
+// Combien des valeurs sont entre 0 et 10
+// Adicional Exibir a soma entre os números ímpares digitados
+//
+//
+
+#include<stdio.h>
+#include<stdlib.h>
+
